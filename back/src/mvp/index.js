0c10bedbdcddbada7e1c6e2395cc4fc7718a9e20
PrettyHan <imbel9830@gmail.com>
2022-04-21 13:30:38 +0900

    [Update] index_Router

--- back/src/mvp/index.js
+++ back/src/mvp/index.js
@@ -1,7 +1,10 @@
 import { userRouter } from "./user/userRouter";
+import { searchRouter } from "./search/searchRouter"
+
 
 const indexRouter = (app) => {
     app.use("/auth", userRouter);
+    app.use("/search", searchRouter)
 };
 
 export { indexRouter };
