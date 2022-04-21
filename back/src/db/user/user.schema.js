078eb222833d93255c0e41e0a9abf0f44c9ec473
PrettyHan <imbel9830@gmail.com>
2022-04-21 16:40:03 +0900

    [Update] User 스키마 토대 완성

--- back/src/db/user/user.schema.js
+++ back/src/db/user/user.schema.js
@@ -4,26 +4,91 @@ const { Schema, model } = pkg;
 
 const UserSchema = new Schema(
     {
-        id: {
+        userId: {
             type: String,
             required: true,
-        },
-        email: {
-            type: String,
-            required: true,
-        },
-        name: {
-            type: String,
-            required: true,
+            unique: 1,
+            trim: true,
         },
         password: {
             type: String,
             required: true,
+            trim: true,
+            validate:[
+                function(password){
+                 return password&&password.length>6;
+                },'비밀번호를 입력하거나 길이가 6보다커야합니다.'
+               ]
         },
-        description: {
+        name: {
             type: String,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: 1,
+            trim: true,
+        },
+        gender: {
+            type: Number,
+            required: true,
+        },
+        Phone: {
+            type: Number,
+            required: true,
+            maxlength: 13,
+            trim: true,
+            match: /^\d{2,3}-\d{3,4}-\d{4}$/
+        },
+        birth: {
+            type: Date,
+            required: true,
+            maxlength: 10,
+            trim: true,
+        },
+        prefer_color: {
+            type: Array,
             required: false,
-            default: "설명이 아직 없습니다. 추가해 주세요.",
+            default: null
+        },
+        zipcode: {
+            type: Object,
+            required: false,
+            default: null,
+            properties: {
+                country: {
+                    type: String,
+                },
+                do: {
+                    type: String
+                },
+                si: {
+                    type: String
+                },
+                gu: {
+                    type: String
+                },
+                ro: {
+                    type: String
+                },
+                rest: {
+                    type: String
+                },
+                
+            }
+            
+        },
+        bookmark: {
+            type: Array,
+            required: false,
+            default: null,
+        },
+        cart: {
+            type: Array,
+            required: false,
+            default: null,
         },
     },
     {
