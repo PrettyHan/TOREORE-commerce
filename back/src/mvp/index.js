ce44d2b698dcf33cc5cfb8d3d0f86d4938f9b62a
haechan <kiki9510@gmail.com>
2022-04-21 16:12:28 +0900

    [Add] 상품리스트 조회, 개별상품 조회 api 뼈대 작성 완료

--- back/src/mvp/index.js
+++ back/src/mvp/index.js
@@ -1,7 +1,9 @@
 import { userRouter } from "./user/userRouter";
+import { productRouter } from "./product/productRouter";
 
 const indexRouter = (app) => {
     app.use("/auth", userRouter);
+    app.use("/products", productRouter);
 };
 
 export { indexRouter };
--- back/src/mvp/product/productRouter.js
+++ back/src/mvp/product/productRouter.js
@@ -2,6 +2,16 @@ import { Router } from "express";
 
 const productRouter = Router();
 
-// productRouter.get("");
+// 모든 상품 정보 리스트 조회
+productRouter.get("/list", (req, res) => {
+    // productList : 상품 리스트 조회
+});
+
+// 개별 상품 정보 조회
+productRouter.get("/:product_id", (req, res) => {
+    const productId = req.params.productId;
+
+    // proudctInfo : productId를 기준으로 상품 정보 DB 조회
+});
 
 export { productRouter };
