e81683f3b62e6cc21073088c214966f534a21d9b
haechan <kiki9510@gmail.com>
2022-04-21 16:17:42 +0900

    [New&Add] productService 생성, 뼈대 코드 작성

--- back/src/mvp/product/productRouter.js
+++ back/src/mvp/product/productRouter.js
@@ -5,6 +5,7 @@ const productRouter = Router();
 // 모든 상품 정보 리스트 조회
 productRouter.get("/list", (req, res) => {
     // productList : 상품 리스트 조회
+    // getProductList
 });
 
 // 개별 상품 정보 조회
@@ -12,6 +13,7 @@ productRouter.get("/:product_id", (req, res) => {
     const productId = req.params.productId;
 
     // proudctInfo : productId를 기준으로 상품 정보 DB 조회
+    // getProduct
 });
 
 export { productRouter };
---
+++ back/src/mvp/product/productService.js
@@ -0,0 +1,9 @@
+class productService {
+    // 전체 상품리스트 조회
+    static async getProductList() {}
+
+    // 개별 상품 정보 조회
+    static async getProduct() {}
+}
+
+export { productService };
