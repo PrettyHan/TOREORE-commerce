695fb97b098d2eb7cd3749367b3347f894fba7f9
jplendor <jplendor@gmail.com>
2022-04-22 23:38:11 +0900

    [Update&New] Category.js 수정(MUI grid 제거), Category.css 추가

--- front/src/components/product/Category.js
+++ front/src/components/product/Category.js
@@ -1,49 +1,25 @@
-import * as React from "react";
-import { styled } from "@mui/material/styles";
-import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
-import Box from "@mui/material/Box";
-
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-}));
+import "../../style/category.css";
 
 const Category = () => {
     // 임시 data
     const category1 = ["여성", "남성", "청소년", "아동", "홈"];
-    const category2 = ["상의", "하의", "신발", "가방", "액세서리", "언더웨어"];
+    const category2 = ["상의", "하의", "신발", "가방", "액세서리"];
 
     return (
-        <div>
-            <Box sx={{ width: "100%" }}>
-                <Grid
-                    container
-                    rowSpacing={1}
-                    columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-                >
-                    {category1.map((item) => (
-                        <Grid item xs={2}>
-                            <Item>{item}</Item>
-                        </Grid>
-                    ))}
-                </Grid>
-                <Grid
-                    container
-                    rowSpacing={1}
-                    columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-                >
-                    {category2.map((item) => (
-                        <Grid item xs={2}>
-                            <Item>{item}</Item>
-                        </Grid>
-                    ))}
-                </Grid>
-            </Box>
-        </div>
+        <>
+            <div className="category-container">
+                {category1.map((item) => (
+                    <div className="category">
+                        <div className="category-btn">{item}</div>
+                        <div className="sub-category">
+                            {category2.map((item) => (
+                                <div className="sub-category-btn">{item}</div>
+                            ))}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </>
     );
 };
 
