c96821ad30ae019336e23bccfc590055b271971f
haechan <kiki9510@gmail.com>
2022-04-22 17:32:15 +0900

    [Del]불필요한 파일 제거

--- back/src/mvp/index.js
+++ back/src/mvp/index.js
@@ -1,9 +1,7 @@
 import { userRouter } from "./user/userRouter";
-import { productRouter } from "./product/productRouter";
 
 const indexRouter = (app) => {
     app.use("/auth", userRouter);
-    app.use("/products", productRouter);
 };
 
 export { indexRouter };
