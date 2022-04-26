b8ba6a7efb97a76b841bad50959c4fb796f02667
haechan <kiki9510@gmail.com>
2022-04-26 16:35:24 +0900

    [Add]장바구니에서 특정 상품 삭제 api 구현

--- back/src/mvp/cart/cartRouter.js
+++ back/src/mvp/cart/cartRouter.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { cartService } from "./cartService";
 import { loginRequired } from "../../middlewares/loginRequired";
 
 const cartRouter = Router();
@@ -19,3 +20,18 @@ cartRouter.get("/", async (req, res, next) => {
         next(error);
     }
 });
+
+cartRouter.delete("/:productId", async (req, res, next) => {
+    try {
+        const userId = req.currentUserId;
+        const productId = req.params.productId;
+
+        const carts = await cartSerivce.deleteProductIdOfCart({ userId, productId });
+
+        res.status(200).json(carts);
+    } catch (error) {
+        next(error);
+    }
+});
+
+export { cartRouter };
