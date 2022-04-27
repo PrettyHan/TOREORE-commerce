3be01e2e6dbb9cc42cf95b11a720d60718579d43
leejeongmin <leejm648@gmail.com>
2022-04-27 20:13:54 +0900

    [Update] Route 추가, product 관련 컴포넌트 구조 변경(App, Main, Category, ProductList)

--- front/src/App.js
+++ front/src/App.js
@@ -11,6 +11,9 @@ import Footer from "./components/Layout/Footer";
 import Introduce from "./components/introduce/Introduce";
 import MyPage from "./components/user/MyPage";
 import Login from "./components/Auth/Login";
+import ProductList from "./components/product/ProductList";
+import Category from "./components/product/Category";
+
 // import RegisterForm from "./components/user/RegisterForm";
 // import Portfolio from "./components/Portfolio";
 // import Loading from "./components/Loading";
@@ -19,80 +22,82 @@ export const UserStateContext = createContext(null);
 export const DispatchContext = createContext(null);
 
 function App() {
-  // useReducer 훅을 통해 userState 상태와 dispatch함수를 생성함.
-  // const [userState, dispatch] = useReducer(loginReducer, {
-  //   user: null,
-  // });
+    // useReducer 훅을 통해 userState 상태와 dispatch함수를 생성함.
+    // const [userState, dispatch] = useReducer(loginReducer, {
+    //   user: null,
+    // });
 
-  // 아래의 fetchCurrentUser 함수가 실행된 다음에 컴포넌트가 구현되도록 함.
-  // 아래 코드를 보면 isFetchCompleted 가 true여야 컴포넌트가 구현됨.
-  // const [isFetchCompleted, setIsFetchCompleted] = useState(false);
+    // 아래의 fetchCurrentUser 함수가 실행된 다음에 컴포넌트가 구현되도록 함.
+    // 아래 코드를 보면 isFetchCompleted 가 true여야 컴포넌트가 구현됨.
+    // const [isFetchCompleted, setIsFetchCompleted] = useState(false);
 
-  // const fetchCurrentUser = async () => {
-  //   try {
-  //     // 이전에 발급받은 토큰이 있다면, 이를 가지고 유저 정보를 받아옴.
-  //     const res = await Api.get("users/current");
-  //     const currentUser = res.data.user;
+    // const fetchCurrentUser = async () => {
+    //   try {
+    //     // 이전에 발급받은 토큰이 있다면, 이를 가지고 유저 정보를 받아옴.
+    //     const res = await Api.get("users/current");
+    //     const currentUser = res.data.user;
 
-  //     // dispatch 함수를 통해 로그인 성공 상태로 만듦.
-  //     dispatch({
-  //       type: "LOGIN_SUCCESS",
-  //       payload: currentUser,
-  //     });
-  //   } catch {
-  //     console.log("%c SessionStorage에 토큰 없음.", "color: #d93d1a;");
-  //   }
+    //     // dispatch 함수를 통해 로그인 성공 상태로 만듦.
+    //     dispatch({
+    //       type: "LOGIN_SUCCESS",
+    //       payload: currentUser,
+    //     });
+    //   } catch {
+    //     console.log("%c SessionStorage에 토큰 없음.", "color: #d93d1a;");
+    //   }
 
-  //   // fetchCurrentUser 과정이 끝났으므로, isFetchCompleted 상태를 true로 바꿔줌
-  //   setIsFetchCompleted(true);
-  // };
+    //   // fetchCurrentUser 과정이 끝났으므로, isFetchCompleted 상태를 true로 바꿔줌
+    //   setIsFetchCompleted(true);
+    // };
 
-  // // useEffect함수를 통해 fetchCurrentUser 함수를 실행함.
-  // useEffect(() => {
-  //   fetchCurrentUser();
-  // }, []);
+    // // useEffect함수를 통해 fetchCurrentUser 함수를 실행함.
+    // useEffect(() => {
+    //   fetchCurrentUser();
+    // }, []);
 
-  // if (!isFetchCompleted) {
-  //   return <Loading />;
-  // }
+    // if (!isFetchCompleted) {
+    //   return <Loading />;
+    // }
 
-  const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false);
 
-  const handleOpen = () => {
-    setOpen(true);
-  };
+    const handleOpen = () => {
+        setOpen(true);
+    };
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-  return (
-    <DispatchContext.Provider>
-      <UserStateContext.Provider>
-        <Router>
-          <Header handleOpen={handleOpen} />
-          <Login open={open} handleClose={handleClose} />
-          <Routes>
-            <Route path="/" exact element={<Main />} />
-            <Route path="/introduce" element={<Introduce />} />
-            <Route path="/myPage" element={<MyPage />} />
-            <Route path="/auth/:id" element={<Main />} />
-            <Route path="*" element={<Main />} />
-          </Routes>
-        </Router>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <Footer />
-      </UserStateContext.Provider>
-    </DispatchContext.Provider>
-  );
+    return (
+        <DispatchContext.Provider>
+            <UserStateContext.Provider>
+                <Router>
+                    <Header handleOpen={handleOpen} />
+                    <Login open={open} handleClose={handleClose} />
+                    <Category />
+                    <Routes>
+                        <Route path="/" exact element={<Main />} />
+                        <Route path="/introduce" element={<Introduce />} />
+                        <Route path="/myPage" element={<MyPage />} />
+                        <Route path="/auth/:id" element={<Main />} />
+                        <Route path="*" element={<Main />} />
+                        <Route path="/products" element={<ProductList />} />
+                    </Routes>
+                </Router>
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <Footer />
+            </UserStateContext.Provider>
+        </DispatchContext.Provider>
+    );
 }
 
 export default App;
