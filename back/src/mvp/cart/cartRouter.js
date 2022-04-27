c62d29ec4fb1b8f13706b7c44abd37da008394d7
haechan <kiki9510@gmail.com>
2022-04-27 11:36:04 +0900

    [Update]유저스키마 cart 필드 디폴트값 변경, getCartList 로직 구현

--- back/src/mvp/cart/cartRouter.js
+++ back/src/mvp/cart/cartRouter.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { cartService } from "./cartService";
 import { loginRequired } from "../../middlewares/loginRequired";
+import { cartService } from "./cartService";
 
 const cartRouter = Router();
 cartRouter.use(loginRequired);
@@ -9,7 +9,7 @@ cartRouter.get("/", async (req, res, next) => {
     try {
         const userId = req.currentUserId;
 
-        const carts = await cartSerivce.getCartList({ userId });
+        const carts = await cartService.getCartList({ userId });
 
         if (carts.errorMessage) {
             throw new Error(carts.errorMessage);
@@ -25,9 +25,9 @@ cartRouter.put("/:productId", async (req, res, next) => {
     try {
         const userId = req.currentUserId;
         const productId = req.params.productId;
-        // body로 수량 몇개로 수정하는지 or 기존에서 몇개 추가/제거 됐는지(+1 or -3 ...)
+        const { quantity } = req.body; // body로 수량 몇개로 수정하는지 or 기존에서 몇개 추가/제거 됐는지(+1 or -3 ...)
 
-        const carts = await cartSerivce.updateCartList({ userId, productId }); // 유저의 카트 리스트 조회 -> 수량 정보 업데이트
+        const carts = await cartService.updateCartList({ userId, productId, quantity });
 
         res.status(200).json(carts);
     } catch (error) {
@@ -40,7 +40,7 @@ cartRouter.delete("/:productId", async (req, res, next) => {
         const userId = req.currentUserId;
         const productId = req.params.productId;
 
-        const carts = await cartSerivce.deleteProductIdOfCart({ userId, productId }); // 유저의 카트 리스트 조회 -> id가 일치하는 product 삭제
+        const carts = await cartService.deleteProductIdOfCart({ userId, productId });
 
         res.status(200).json(carts);
     } catch (error) {
--- back/src/mvp/cart/cartService.js
+++ back/src/mvp/cart/cartService.js
@@ -1,10 +1,16 @@
 import { User, Product } from "../../db";
 
 class cartService {
-    static async getCartList({ userId }) {}
+    // 유저의 카트 리스트 조회
+    static async getCartList({ userId }) {
+        const user = await User.findByUserId({ userId });
+        return user.cart; // array
+    }
 
-    static async updateCartList({ userId, productId }) {}
+    // 유저의 카트 리스트 조회 -> 수량 정보 업데이트
+    static async updateCartList({ userId, productId, quantity }) {}
 
+    // 유저의 카트 리스트 조회 -> id가 일치하는 product 삭제
     static async deleteProductIdOfCart({ userId, productId }) {}
 }
 
