e2f28b215694cb20f4933a822fb3d639bbfe3ba9
Hayeong Kim <fibonacci2246@gmail.com>
2022-04-28 22:42:12 +0900

    [fix, add] 디자인 및 로직 수정, 선택삭제/계속쇼핑 버튼 추가

--- front/src/App.js
+++ front/src/App.js
@@ -13,9 +13,10 @@ import MyPage from "./components/user/MyPage";
 import Login from "./components/Auth/Login";
 import ProductList from "./components/product/ProductList";
 import Category from "./components/product/Category";
+import Cart from "./components/cart/Cart";
 
 const ProductDetail = React.lazy(() =>
-    import("./components/product/ProductDetail")
+  import("./components/product/ProductDetail")
 );
 
 // import RegisterForm from "./components/user/RegisterForm";
@@ -26,88 +27,89 @@ export const UserStateContext = createContext(null);
 export const DispatchContext = createContext(null);
 
 function App() {
-    // useReducer 훅을 통해 userState 상태와 dispatch함수를 생성함.
-    const [userState, dispatch] = useReducer(loginReducer, {
-        user: null,
-    });
+  // useReducer 훅을 통해 userState 상태와 dispatch함수를 생성함.
+  const [userState, dispatch] = useReducer(loginReducer, {
+    user: null,
+  });
 
-    // 아래의 fetchCurrentUser 함수가 실행된 다음에 컴포넌트가 구현되도록 함.
-    // 아래 코드를 보면 isFetchCompleted 가 true여야 컴포넌트가 구현됨.
-    const [isFetchCompleted, setIsFetchCompleted] = useState(false);
-    const [open, setOpen] = useState(false);
+  // 아래의 fetchCurrentUser 함수가 실행된 다음에 컴포넌트가 구현되도록 함.
+  // 아래 코드를 보면 isFetchCompleted 가 true여야 컴포넌트가 구현됨.
+  const [isFetchCompleted, setIsFetchCompleted] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    const fetchCurrentUser = async () => {
-        try {
-            // 이전에 발급받은 토큰이 있다면, 이를 가지고 유저 정보를 받아옴.
-            const res = await Api.get("auth/user");
-            const currentUser = res.data;
+  const fetchCurrentUser = async () => {
+    try {
+      // 이전에 발급받은 토큰이 있다면, 이를 가지고 유저 정보를 받아옴.
+      const res = await Api.get("auth/user");
+      const currentUser = res.data;
 
-            // dispatch 함수를 통해 로그인 성공 상태로 만듦.
-            dispatch({
-                type: "LOGIN_SUCCESS",
-                payload: currentUser,
-            });
-        } catch {
-            console.log("%c SessionStorage에 토큰 없음.", "color: #d93d1a;");
-        }
-
-        // fetchCurrentUser 과정이 끝났으므로, isFetchCompleted 상태를 true로 바꿔줌
-        setIsFetchCompleted(true);
-    };
-
-    // useEffect함수를 통해 fetchCurrentUser 함수를 실행함.
-    useEffect(() => {
-        fetchCurrentUser();
-    }, []);
-
-    if (!isFetchCompleted) {
-        return "isLoading...";
+      // dispatch 함수를 통해 로그인 성공 상태로 만듦.
+      dispatch({
+        type: "LOGIN_SUCCESS",
+        payload: currentUser,
+      });
+    } catch {
+      console.log("%c SessionStorage에 토큰 없음.", "color: #d93d1a;");
     }
 
-    const handleOpen = () => {
-        setOpen(true);
-    };
+    // fetchCurrentUser 과정이 끝났으므로, isFetchCompleted 상태를 true로 바꿔줌
+    setIsFetchCompleted(true);
+  };
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+  // useEffect함수를 통해 fetchCurrentUser 함수를 실행함.
+  useEffect(() => {
+    fetchCurrentUser();
+  }, []);
 
-    return (
-        <DispatchContext.Provider value={dispatch}>
-            <UserStateContext.Provider value={userState}>
-                <React.Suspense fallback={<div>Loading...</div>}>
-                    <Router>
-                        <Header handleOpen={handleOpen} />
-                        <Login open={open} handleClose={handleClose} />
-                        <Category />
-                        <Routes>
-                            <Route path="/" exact element={<Main />} />
-                            <Route path="/introduce" element={<Introduce />} />
-                            <Route path="/myPage" element={<MyPage />} />
-                            <Route path="/auth/:id" element={<Main />} />
-                            <Route path="*" element={<Main />} />
-                            <Route path="/products" element={<ProductList />} />
-                            <Route
-                                exact
-                                path="/products/:categoryId/:productId"
-                                element={<ProductDetail />}
-                            />
-                        </Routes>
-                    </Router>
-                </React.Suspense>
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <Footer />
-            </UserStateContext.Provider>
-        </DispatchContext.Provider>
-    );
+  if (!isFetchCompleted) {
+    return "isLoading...";
+  }
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <DispatchContext.Provider value={dispatch}>
+      <UserStateContext.Provider value={userState}>
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <Router>
+            <Header handleOpen={handleOpen} />
+            <Login open={open} handleClose={handleClose} />
+            <Category />
+            <Routes>
+              <Route path="/" exact element={<Main />} />
+              <Route path="/introduce" element={<Introduce />} />
+              <Route path="/myPage" element={<MyPage />} />
+              <Route path="/auth/:id" element={<Main />} />
+              <Route path="*" element={<Main />} />
+              <Route path="/products" element={<ProductList />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route
+                exact
+                path="/products/:categoryId/:productId"
+                element={<ProductDetail />}
+              />
+            </Routes>
+          </Router>
+        </React.Suspense>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <Footer />
+      </UserStateContext.Provider>
+    </DispatchContext.Provider>
+  );
 }
 
 export default App;
--- front/src/components/cart/Cart.js
+++ front/src/components/cart/Cart.js
@@ -1,12 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Typography, Grid, Box, Button, Checkbox } from "@mui/material/";
+import {
+  Typography,
+  Grid,
+  Box,
+  Button,
+  Checkbox,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+} from "@mui/material/";
 import CartItem from "./CartItem";
+import CartTableCell from "./CartTableCell";
 
 import * as Api from "../../api";
 
 function Cart() {
   const navigate = useNavigate();
+
+  const theme = {};
 
   const fakeData = [
     {
@@ -36,22 +50,12 @@ function Cart() {
   ];
 
   const [cartItems, setCartItems] = useState(fakeData);
-
-  const fetchCartItems = async () => {
-    try {
-      const res = await Api.get("carts");
-
-      setCartItems(res.data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  useEffect(() => {
-    fetchCartItems();
-  }, []);
-
-  const cartItemList = cartItems.map((cartItem) => (
-    <CartItem cartItem={cartItem} setCartItems={setCartItems} />
+  const cartItemList = cartItems.map((cartItem, index) => (
+    <CartItem
+      cartItem={cartItem}
+      setCartItems={setCartItems}
+      index={index + 1}
+    />
   ));
 
   const checkedCartItems = cartItems.filter(
@@ -62,32 +66,151 @@ function Cart() {
     0
   );
 
+  const isCartEmpty = Array.isArray(cartItems) && cartItems.length === 0;
+  const isCheckedAll = (cartItems) => {
+    if (cartItems.some((cartItem) => cartItem.checked === false)) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const handleCartData = (cartItems) => {
+    return cartItems.map((item) => {
+      return {
+        ...item,
+        checked: true,
+      };
+    });
+  };
+
+  const handleCheck = (event) => {
+    setCartItems((current) => {
+      return current.map((item) => {
+        return {
+          ...item,
+          checked: event.target.checked,
+        };
+      });
+    });
+  };
+
+  const handleSelectRemove = async () => {
+    try {
+      // const deleteProducts = checkedCartItems.map((item) => {
+      //   return item.productId
+      // })
+      // await Api.del("carts/select", {
+      //   productIdArr: deleteProducts
+      // });
+
+      setCartItems((current) => {
+        return current.filter((item) => item.checked === false);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleOrder = async () => {
+    try {
+      const orderProduts = checkedCartItems.map((item) => {
+        return {
+          productId: item.productId,
+          quantity: item.quantity,
+        };
+      });
+      const res = await Api.post("orders", orderProduts);
+
+      const orderId = res.data.orderId;
+      navigate(`/order/${orderId}`);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const fetchCartItems = async () => {
+    try {
+      const res = await Api.get("carts");
+
+      setCartItems(handleCartData(res.data));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchCartItems();
+  }, []);
+
   return (
     <div>
-      <Box>
+      {/* <Box>
         <Grid>
-          <Typography>장바구니</Typography>
         </Grid>
         <Grid>
-          <Checkbox>모두 선택</Checkbox>
-          {Array.isArray(cartItems) && cartItems.length === 0 ? (
+        <Checkbox>모두 선택</Checkbox>
+        
+            <Button>선택 삭제</Button>
+            </Grid>
+            <Grid>
+            <Typography>총 결제 금액: {carculateTotal}원</Typography>
+            </Grid>
+            <Grid>
+            <Button variant="text" onClick={() => navigate("/order")}>
+            주문하기
+            </Button>
+            </Grid>
+          </Box> */}
+
+      <Box>
+        <Typography component="h2" variant="h6" color="primary" gutterBottom>
+          장바구니
+        </Typography>
+      </Box>
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <CartTableCell>전체 {cartItems.length}개</CartTableCell>
+            <CartTableCell>
+              <Checkbox
+                checked={isCheckedAll(cartItems)}
+                onChange={handleCheck}
+              ></Checkbox>
+            </CartTableCell>
+            <CartTableCell>이미지</CartTableCell>
+            <CartTableCell>상품명</CartTableCell>
+            <CartTableCell>판매가</CartTableCell>
+            <CartTableCell>주문금액</CartTableCell>
+            <CartTableCell>수량</CartTableCell>
+            <CartTableCell>주문관리</CartTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {!isCartEmpty ? (
             cartItemList
           ) : (
             <Typography>장바구니에 상품을 담지 않았습니다.</Typography>
           )}
-          <Button>선택 삭제</Button>
-        </Grid>
-        <Grid>
-          <Typography>총 결제 금액: {carculateTotal}원</Typography>
-        </Grid>
-        <Grid>
-          <Button
-            variant="text"
-            onClick={() => navigate("/order", { cartItems: checkedCartItems })}
-          >
-            결제하기
-          </Button>
-        </Grid>
+        </TableBody>
+      </Table>
+      <Box>
+        <Typography>총 결제 금액: {carculateTotal}원</Typography>
+      </Box>
+      <Box>
+        <Button
+          disabled={isCartEmpty || !isCheckedAll(cartItems)}
+          onClick={handleSelectRemove}
+        >
+          선택삭제
+        </Button>
+        <Button
+          disabled={isCartEmpty || !isCheckedAll(cartItems)}
+          onClick={handleOrder}
+        >
+          주문하기
+        </Button>
+        <Button onClick={() => navigate(-1)}>쇼핑 계속하기</Button>
       </Box>
     </div>
   );
