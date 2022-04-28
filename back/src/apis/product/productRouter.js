c8e7a24685a5821e1d236caa4909fc8143cd126c
PrettyHan <imbel9830@gmail.com>
2022-04-28 17:13:43 +0900

    [Update] product_MVP_쿼리 검색기능 구현

--- back/src/apis/product/productRouter.js
+++ back/src/apis/product/productRouter.js
@@ -5,15 +5,27 @@ import { productService } from "./productService";
 const productRouter = Router();
 
 // product 전체 조회
+
 productRouter.get("/", async function (req, res, next) {
     try {
-        const products = await productService.getProductList();
+        const {cid, pid} = req.query
+        if (cid || pid) {
+            const product = await productService.getProductByQuery({cid, pid});
 
+            if (product.errorMessage) {
+                throw new Error(product.errorMessage);
+            }
+    
+            res.status(200).send(product);
+        }
+        else {
+            const products = await productService.getProductList();
+        
         if (products.errorMessage) {
             throw new Error(products.errorMessage);
         }
-
-        res.status(200).json(products);
+        res.status(200).send(products);
+    }
     } catch (error) {
         next(error);
     }
@@ -34,38 +46,5 @@ productRouter.get("/:productId", async function (req, res, next) {
         next(error);
     }
 });
-
-productRouter.get("/?category=category", async function (req, res, next) {
-    try {
-        const categoryQuery = req.query;
-        const products = await productService.getProductByQuery(categoryQuery);
-
-        if (products.errorMessage) {
-            throw new Error(products.errorMessage);
-        }
-
-        res.status(200).json(products);
-    } catch (error) {
-        next(error);
-    }
-});
-
-productRouter.get(
-    "/?category=category&productId=productId",
-    async function (req, res, next) {
-        try {
-            const categoryQuery = req.query;
-            const product = await productService.getProductByQuery(categoryQuery);
-
-            if (product.errorMessage) {
-                throw new Error(product.errorMessage);
-            }
-
-            res.status(200).json(product);
-        } catch (error) {
-            next(error);
-        }
-    },
-);
 
 export { productRouter };
