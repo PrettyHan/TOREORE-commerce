f8e81ad4aac1be895381bb2e29b16a43f592a65d
JunSeokCheon <goosw775@naver.com>
2022-04-28 11:16:24 +0900

    [Update] Merge 수정

--- back/src/mvp/index.js
+++ back/src/mvp/index.js
@@ -1,10 +1,12 @@
 import { userRouter } from "./user/userRouter";
 import { productRouter } from "./product/productRouter";
 import { likeRouter } from "./like/likeRouter"
+import { orderRouter } from "./order/orderRouter"
 
 const indexRouter = (app) => {
     app.use("/auth", userRouter);
     app.use("/products", productRouter)
+    app.use("/orders", orderRouter)
     app.use("/likes", likeRouter)
 };
 
