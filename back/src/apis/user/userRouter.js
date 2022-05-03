d644423dc72a715ee60a3911558390ca3ec63f86
haechan <kiki9510@gmail.com>
2022-05-03 19:37:55 +0900

    [Udpate]구글로그인 분기점 업데이트

--- back/src/apis/user/userRouter.js
+++ back/src/apis/user/userRouter.js
@@ -43,7 +43,6 @@ userRouter.post("/signup", async (req, res, next) => {
 // google login
 userRouter.get(
     "/google",
-    cors(),
     passport.authenticate("google", { scope: ["profile", "email"] }),
 );
 
@@ -52,8 +51,19 @@ userRouter.get(
     passport.authenticate("google", { failureRedirect: "http://localhost:3000" }),
     (req, res) => {
         const { user, isMember } = req.user;
-        const accessToken = createAccessToken({ userId: user.userId });
-        res.status(200).json({ accessToken, userInfo: req.user });
+        // const accessToken = createAccessToken({ userId: user.userId });
+        // res.status(200).json({ accessToken, userInfo: req.user });
+        if (isMember === false || (isMember && user.hasAddtionalInfo === false)) {
+            // 추가정보입력 프론트 페이지 이동 -> 정보 입력 후 확인 시, 유저 업데이트 및 토큰 발급 -> 응답
+            res.status(302).redirect(
+                `http://localhost:3000/useredit?uid=${user.userId}`,
+            ); // http://localhost:3000/useredit?uid=103495610959074788067
+        } else {
+            // (추가정보입력 안해도 되는 유저가)로그인 성공 시 -> 로그인 확인 페이지로 이동 -> 토큰 발급
+            res.status(302).redirect(
+                `http://localhost:3000/login-confirm?uid=${user.userId}`,
+            );
+        }
     },
 );
 
@@ -74,6 +84,51 @@ userRouter.post("/login", async (req, res, next) => {
     }
 });
 
+// 소셜로그인 유저 재로그인시 -> 토큰 발급 라우터
+userRouter.post("/get-token", (req, res, next) => {
+    try {
+        const userId = req.body.userId;
+        const accessToken = createAccessToken({ userId });
+
+        res.status(200).json(accessToken);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// 소셜로그인한 유저가 유저정보를 입력하면, 유저정보 업데이트 & 토큰발급 해주는 라우터
+userRouter.put("/social-user", async (req, res, next) => {
+    try {
+        const userId = req.params.userId;
+        const name = req.body.name ?? null;
+        const password = req.body.password ?? null;
+        const gender = req.body.gender ?? null;
+        const phone = req.body.phone ?? null;
+        const birth = req.body.birth ?? null;
+
+        const toUpdate = {
+            password,
+            name,
+            gender,
+            phone,
+            birth,
+        };
+        const updatedUser = await userService.updateUser({
+            userId,
+            toUpdate,
+        });
+
+        if (updatedUser.errorMessage) {
+            throw new Error(updatedUser.errorMessage);
+        }
+
+        res.status(200).json({ accessToken: updatedUser.accessToken });
+    } catch (error) {
+        next(error);
+    }
+});
+
+// 일반 회원수정
 userRouter.put("/user", loginRequired, async (req, res, next) => {
     try {
         const userId = req.currentUserId;
@@ -98,11 +153,6 @@ userRouter.put("/user", loginRequired, async (req, res, next) => {
 
         if (updatedUser.errorMessage) {
             throw new Error(updatedUser.errorMessage);
-        }
-
-        if (updatedUser.ourAccessToken) {
-            res.status(200).json({ accessToken: updatedUser.ourAccessToken });
-            return;
         }
 
         res.status(200).json(updatedUser);
