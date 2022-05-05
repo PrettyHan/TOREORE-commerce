8b383ccfe2b27dd832f1686b0cfa70fed147af6f
haechan <kiki9510@gmail.com>
2022-05-05 15:08:00 +0900

    [Add]상품추천을 위한 새 데이터 업데이트, 선호하는 연령대 추출을 위한 유틸함수 추가

---
+++ back/src/util/calculateProductData.js
@@ -0,0 +1,8 @@
+export function getBestPreferAge(age_index) {
+    if (age_index === 0) return 10;
+    else if (age_index === 1) return 20;
+    else if (age_index === 2) return 30;
+    else if (age_index === 3) return 40;
+    else if (age_index === 4) return 50;
+    else return 60;
+}
