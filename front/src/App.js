a8e4c7a3501a86b489264e085e268298d4e9a47d
Hayeong Kim <fibonacci2246@gmail.com>
2022-05-07 18:13:24 +0900

    [fix] 바로구매 및 주문페이지 수정

--- front/src/App.js
+++ front/src/App.js
@@ -23,7 +23,7 @@ import ArrowUpwardTwoToneIcon from "@mui/icons-material/ArrowUpwardTwoTone";
 import styled from "styled-components";
 
 const ProductDetail = React.lazy(() =>
-    import("./components/product/ProductDetail")
+  import("./components/product/ProductDetail")
 );
 
 // import RegisterForm from "./components/user/RegisterForm";
@@ -34,161 +34,149 @@ export const UserStateContext = createContext(null);
 export const DispatchContext = createContext(null);
 
 function App() {
-    const [scrollY, setScrollY] = useState(0);
-    const [topBtnShow, setTopBtnShow] = useState(false);
+  const [scrollY, setScrollY] = useState(0);
+  const [topBtnShow, setTopBtnShow] = useState(false);
 
-    const handleFollow = () => {
-        setScrollY(window.pageYOffset);
-    };
+  const handleFollow = () => {
+    setScrollY(window.pageYOffset);
+  };
 
-    const scrollToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth",
-        });
-        setScrollY(0);
-        setTopBtnShow(false);
-    };
-
-    // useReducer 훅을 통해 userState 상태와 dispatch함수를 생성함.
-    const [userState, dispatch] = useReducer(loginReducer, {
-        user: null,
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
     });
+    setScrollY(0);
+    setTopBtnShow(false);
+  };
 
-    // 아래의 fetchCurrentUser 함수가 실행된 다음에 컴포넌트가 구현되도록 함.
-    // 아래 코드를 보면 isFetchCompleted 가 true여야 컴포넌트가 구현됨.
-    const [isFetchCompleted, setIsFetchCompleted] = useState(false);
-    // 로그인 다이얼로그를 여는지
-    const [open, setOpen] = useState(false);
+  // useReducer 훅을 통해 userState 상태와 dispatch함수를 생성함.
+  const [userState, dispatch] = useReducer(loginReducer, {
+    user: null,
+  });
 
-    const fetchCurrentUser = async () => {
-        try {
-            // 이전에 발급받은 토큰이 있다면, 이를 가지고 유저 정보를 받아옴.
-            const res = await Api.get("auth/user");
-            const currentUser = res.data;
+  // 아래의 fetchCurrentUser 함수가 실행된 다음에 컴포넌트가 구현되도록 함.
+  // 아래 코드를 보면 isFetchCompleted 가 true여야 컴포넌트가 구현됨.
+  const [isFetchCompleted, setIsFetchCompleted] = useState(false);
+  // 로그인 다이얼로그를 여는지
+  const [open, setOpen] = useState(false);
 
-            // dispatch 함수를 통해 로그인 성공 상태로 만듦.
-            dispatch({
-                type: "LOGIN_SUCCESS",
-                payload: currentUser,
-            });
-        } catch {
-            console.log("%c SessionStorage에 토큰 없음.", "color: #d93d1a;");
-        }
+  const fetchCurrentUser = async () => {
+    try {
+      // 이전에 발급받은 토큰이 있다면, 이를 가지고 유저 정보를 받아옴.
+      const res = await Api.get("auth/user");
+      const currentUser = res.data;
 
-        // fetchCurrentUser 과정이 끝났으므로, isFetchCompleted 상태를 true로 바꿔줌
-        setIsFetchCompleted(true);
-    };
-
-    // useEffect함수를 통해 fetchCurrentUser 함수를 실행함.
-    useEffect(() => {
-        fetchCurrentUser();
-
-        const watch = () => {
-            window.addEventListener("scroll", handleFollow);
-        };
-        watch();
-        return () => {
-            window.removeEventListener("scroll", handleFollow);
-        };
-    }, []);
-
-    useEffect(() => {
-        if (scrollY > 200) {
-            setTopBtnShow(true);
-        } else {
-            setTopBtnShow(false);
-        }
-    }, [scrollY]);
-
-    if (!isFetchCompleted) {
-        return "isLoading...";
+      // dispatch 함수를 통해 로그인 성공 상태로 만듦.
+      dispatch({
+        type: "LOGIN_SUCCESS",
+        payload: currentUser,
+      });
+    } catch {
+      console.log("%c SessionStorage에 토큰 없음.", "color: #d93d1a;");
     }
 
-    const handleOpen = () => {
-        setOpen(true);
+    // fetchCurrentUser 과정이 끝났으므로, isFetchCompleted 상태를 true로 바꿔줌
+    setIsFetchCompleted(true);
+  };
+
+  // useEffect함수를 통해 fetchCurrentUser 함수를 실행함.
+  useEffect(() => {
+    fetchCurrentUser();
+
+    const watch = () => {
+      window.addEventListener("scroll", handleFollow);
     };
-
-    const handleClose = () => {
-        setOpen(false);
+    watch();
+    return () => {
+      window.removeEventListener("scroll", handleFollow);
     };
+  }, []);
 
-    return (
-        <DispatchContext.Provider value={dispatch}>
-            <UserStateContext.Provider value={userState}>
-                <React.Suspense fallback={<div>Loading...</div>}>
-                    <Router>
-                        <Header handleOpen={handleOpen} />
-                        <Login open={open} handleClose={handleClose} />
+  useEffect(() => {
+    if (scrollY > 200) {
+      setTopBtnShow(true);
+    } else {
+      setTopBtnShow(false);
+    }
+  }, [scrollY]);
 
-                        <Routes>
-                            <Route element={<Category />}>
-                                <Route element={<Search />}>
-                                    <Route path="/" exact element={<Main />} />
-                                    <Route
-                                        path="/myPage"
-                                        element={<MyPage />}
-                                    />
-                                    <Route
-                                        path="/useredit"
-                                        element={<UserEdit />}
-                                    />
-                                    <Route
-                                        path="/auth/:id"
-                                        element={<Main />}
-                                    />
-                                    <Route path="*" element={<NotFound />} />
-                                    <Route
-                                        exact
-                                        path="/products/:category"
-                                        element={<ProductList />}
-                                    />
-                                    <Route
-                                        exact
-                                        path="/products/search/:keyword"
-                                        element={<ProductList />}
-                                    />
-                                    <Route
-                                        exact
-                                        path="/products/:category/:productId"
-                                        element={<ProductDetail />}
-                                    />
-                                    <Route path="/cart" element={<Cart />} />
-                                    <Route
-                                        path="/order/:orderId"
-                                        element={<Order />}
-                                    />
-                                    <Route
-                                        path="/order/:orderId/complete"
-                                        element={<OrderComplete />}
-                                    />
-                                </Route>
-                            </Route>
-                            <Route path="/introduce" element={<Introduce />} />
-                        </Routes>
-                        {topBtnShow && (
-                            <TopBtn onClick={scrollToTop}>
-                                <ArrowUpwardTwoToneIcon />
-                            </TopBtn>
-                        )}
-                        <Footer />
-                    </Router>
-                </React.Suspense>
-            </UserStateContext.Provider>
-        </DispatchContext.Provider>
-    );
+  if (!isFetchCompleted) {
+    return "isLoading...";
+  }
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <DispatchContext.Provider value={dispatch}>
+      <UserStateContext.Provider value={userState}>
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <Router>
+            <Header handleOpen={handleOpen} />
+            <Login open={open} handleClose={handleClose} />
+
+            <Routes>
+              <Route element={<Category />}>
+                <Route element={<Search />}>
+                  <Route path="/" exact element={<Main />} />
+                  <Route path="/myPage" element={<MyPage />} />
+                  <Route path="/useredit" element={<UserEdit />} />
+                  <Route path="/auth/:id" element={<Main />} />
+                  <Route path="*" element={<NotFound />} />
+                  <Route
+                    exact
+                    path="/products/:category"
+                    element={<ProductList />}
+                  />
+                  <Route
+                    exact
+                    path="/products/search/:keyword"
+                    element={<ProductList />}
+                  />
+                  <Route
+                    exact
+                    path="/products/:category/:productId"
+                    element={<ProductDetail />}
+                  />
+                  <Route path="/cart" element={<Cart />} />
+                  <Route path="/order/:orderId" element={<Order />} />
+                  <Route
+                    path="/order/:orderId/complete"
+                    element={<OrderComplete />}
+                  />
+                </Route>
+              </Route>
+              <Route path="/introduce" element={<Introduce />} />
+            </Routes>
+            {topBtnShow && (
+              <TopBtn onClick={scrollToTop}>
+                <ArrowUpwardTwoToneIcon />
+              </TopBtn>
+            )}
+            <Footer />
+          </Router>
+        </React.Suspense>
+      </UserStateContext.Provider>
+    </DispatchContext.Provider>
+  );
 }
 
 export default App;
 
 const TopBtn = styled.button`
-    background-color: #5e5b52;
-    position: fixed;
-    bottom: 50px;
-    right: 50px;
-    width: 50px;
-    height: 50px;
-    border-radius: 100%;
-    color: white;
-    border: none;
+  background-color: #5e5b52;
+  position: fixed;
+  bottom: 50px;
+  right: 50px;
+  width: 50px;
+  height: 50px;
+  border-radius: 100%;
+  color: white;
+  border: none;
 `;
